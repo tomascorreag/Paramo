2495bea58be1f7e62de1aced28cbea001adacc8c
tomascorreag <t.correa@uniandes.edu.co>
2026-05-25 21:42:36 -0500

    hide hud

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779757098|96439386';
+const CACHE_VERSION = '1779763300|6125025';
 /** @type {string} */
 const CACHE_PREFIX = 'Paramo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
