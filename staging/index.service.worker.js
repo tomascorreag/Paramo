4659da2810c02b03bc05602369100a0c9e4fa1b0
tomascorreag <tomascorreag@users.noreply.github.com>
2026-07-20 23:21:56 +0000

    deploy: staging -> /staging (88c0c084b840f88af682a71951cc28eb305f908c) 88c0c084b840f88af682a71951cc28eb305f908c

--- staging/index.service.worker.js
+++ staging/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784134202|3527123';
+const CACHE_VERSION = '1784589710|3702634';
 /** @type {string} */
 const CACHE_PREFIX = 'Paramo-staging-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
