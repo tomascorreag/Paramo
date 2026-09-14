c4a5ef6119cc47e5818d29e61e7287f57381fff5
tomascorreag <tomascorreag@users.noreply.github.com>
2026-09-14 22:04:53 +0000

    deploy: main -> / (8a31e519dca28a362cfd761ef184a5caf31f0629) 8a31e519dca28a362cfd761ef184a5caf31f0629

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787588374|4105145';
+const CACHE_VERSION = '1789423487|4426820';
 /** @type {string} */
 const CACHE_PREFIX = 'Paramo-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
